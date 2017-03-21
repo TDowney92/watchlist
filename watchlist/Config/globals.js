f0c5c87b3e6a093031da2377e8b649b3eb6624ba
TDowney92 <tmed425@hotmail.com>
2017-03-21 17:56:36 -0400

    database stuff

---
+++ watchlist/Config/globals.js
@@ -0,0 +1,4 @@
+// create an array of global variables
+module.exports = {
+	db:'mongodb://tmed425:misty425@ds137360.mlab.com:37360/watchlistdb' //mlab database connection
+};
--- watchlist/routes/index.js
+++ watchlist/routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+		title: 'Watch List' ,
+		message: 'Media Checklist'
+	});
 });
+
+/* GET register */
+router.get('/register', function(req, res, next) {
+	res.render('register', {
+		title: 'Please Register with Watchlist'
+	});
+});
+
+/* GET login */
+router.get('/login', function(req, res, next) {
+
+	// create a variable to store any login message
+	let messages = req.sessions.messages || [];
+
+	// clear the session messages
+	req.session.messages = [];
+
+	res.render('login', {
+		title: 'Please login into Watchlist'
+	});
+});
+
 
 module.exports = router;
